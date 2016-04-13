154ee1e5b4a42d3ae076a79f4b919332959e5d5b
Affan Salman <affan.salman@openscg.com>
2016-04-13 17:45:37 +0100

    Clean up and organize imports for the test program.

--- Test/Cli/HiveJdbcClient.java
+++ Test/Cli/HiveJdbcClient.java
@@ -1,10 +1,8 @@
-import java.sql.*;
-import java.sql.SQLException;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.DriverManager;
-import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HiveJdbcClient {
 
